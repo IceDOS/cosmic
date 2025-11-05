5c0cbd9eb1bb0c07358e8cee86546ecd79e185e6
IceDBorn <git.outsider841@simplelogin.fr>
2025-11-05 05:10:22 +0200

    rework(modules): replace accent-color with theme-mode and add gtk theming

---
+++ modules/desktop/appearance/gtk-theming/icedos.nix
@@ -0,0 +1,39 @@
+{ icedosLib, ... }:
+
+{
+  options.icedos.desktop.cosmic.gtk-theming =
+    let
+      inherit (icedosLib) mkBoolOption;
+    in
+    mkBoolOption { default = true; };
+
+  outputs.nixosModules =
+    { ... }:
+    [
+      (
+        {
+          config,
+          lib,
+          ...
+        }:
+        let
+          inherit (config.icedos) users;
+          inherit (lib) mapAttrs;
+
+          force = true;
+          text = "true";
+        in
+        {
+          home-manager.users = mapAttrs (user: _: {
+            home.file = {
+              ".config/cosmic/com.system76.CosmicTk/v1/apply_theme_global" = {
+                inherit force text;
+              };
+            };
+          }) users;
+        }
+      )
+    ];
+
+  meta.name = "gtk-theming";
+}
