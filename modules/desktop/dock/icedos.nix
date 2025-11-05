1da0674f1beb77150d2bcd81f52bd86dff1eb180
IceDBorn <git.outsider841@simplelogin.fr>
2025-11-05 16:06:17 +0200

    feat(modules): add dock

---
+++ modules/desktop/dock/icedos.nix
@@ -0,0 +1,171 @@
+{ icedosLib, lib, ... }:
+
+{
+  options.icedos.desktop.cosmic.dock =
+    let
+      inherit (icedosLib)
+        mkBoolOption
+        mkNumberOption
+        mkStrListOption
+        mkStrOption
+        ;
+
+      inherit
+        (
+          let
+            inherit (lib) readFile;
+          in
+          (fromTOML (readFile ./config.toml)).icedos.desktop.cosmic.dock
+        )
+        autohide
+        enable
+        expand
+        gaps
+        monitor
+        opacity
+        plugins
+        position
+        size
+        themeMode
+        ;
+
+      inherit (plugins) center left right;
+    in
+    {
+      enable = mkBoolOption { default = enable; };
+      autohide = mkBoolOption { default = autohide; };
+      expand = mkBoolOption { default = expand; };
+      gaps = mkBoolOption { default = gaps; };
+      monitor = mkStrOption { default = monitor; };
+      opacity = mkNumberOption { default = opacity; };
+
+      plugins = {
+        center = mkStrListOption { default = center; };
+        left = mkStrListOption { default = left; };
+        right = mkStrListOption { default = right; };
+      };
+
+      position = mkStrOption { default = position; };
+      size = mkStrOption { default = size; };
+      themeMode = mkStrOption { default = themeMode; };
+    };
+
+  outputs.nixosModules =
+    { ... }:
+    [
+      (
+        {
+          config,
+          lib,
+          ...
+        }:
+        let
+          inherit (config.icedos) desktop users;
+          inherit (desktop) cosmic;
+
+          inherit (cosmic.dock)
+            autohide
+            enable
+            expand
+            gaps
+            monitor
+            opacity
+            plugins
+            position
+            size
+            themeMode
+            ;
+
+          inherit (plugins) center left right;
+
+          inherit (lib) concatMapStringsSep mapAttrs mkIf;
+          force = true;
+        in
+        {
+          home-manager.users = mapAttrs (user: _: {
+            home.file = {
+              ".config/cosmic/com.system76.CosmicPanel.Dock/v1/anchor" = {
+                inherit force;
+                text = position;
+              };
+
+              ".config/cosmic/com.system76.CosmicPanel.Dock/v1/anchor_gap" = {
+                inherit force;
+                text = if gaps then "true" else "false";
+              };
+
+              ".config/cosmic/com.system76.CosmicPanel.Dock/v1/autohide" = {
+                inherit force;
+                text =
+                  if autohide then
+                    ''
+                      Some((
+                          wait_time: 1000,
+                          transition_time: 200,
+                          handle_size: 4,
+                          unhide_delay: 200,
+                      ))
+                    ''
+                  else
+                    "None";
+              };
+
+              ".config/cosmic/com.system76.CosmicPanel.Dock/v1/background" = {
+                inherit force;
+                text = themeMode;
+              };
+
+              ".config/cosmic/com.system76.CosmicPanel/v1/entries" = {
+                inherit force;
+                text = if !enable then ''["Panel"]'' else ''["Panel", "Dock",]'';
+              };
+
+              ".config/cosmic/com.system76.CosmicPanel.Dock/v1/expand_to_edges" = {
+                inherit force;
+                text = if expand then "true" else "false";
+              };
+
+              ".config/cosmic/com.system76.CosmicPanel.Dock/v1/plugins_center" = {
+                inherit force;
+
+                text = ''
+                  Some([
+                      ${(concatMapStringsSep "" (plugin: ''"${plugin}",'') center)}
+                  ])
+                '';
+              };
+
+              ".config/cosmic/com.system76.CosmicPanel.Dock/v1/plugins_wings" = {
+                inherit force;
+
+                text = ''
+                  Some(([
+                      ${(concatMapStringsSep "" (plugin: ''"${plugin}",'') left)}
+                  ], [
+                      ${(concatMapStringsSep "" (plugin: ''"${plugin}",'') right)}
+                  ]))
+                '';
+              };
+
+              ".config/cosmic/com.system76.CosmicPanel.Dock/v1/opacity" = {
+                inherit force;
+                text = toString (opacity / 100);
+              };
+
+              ".config/cosmic/com.system76.CosmicPanel.Dock/v1/output" = mkIf (monitor != "") {
+                inherit force;
+                text = ''Name("${monitor}")'';
+              };
+
+              ".config/cosmic/com.system76.CosmicPanel.Dock/v1/size" = {
+                inherit force;
+                text = size;
+              };
+            };
+          }) users;
+        }
+      )
+    ];
+
+  meta.name = "dock";
+}
