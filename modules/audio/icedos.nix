0df2601cc5b57d5b0891023f456dce10003b91b5
IceDBorn <git.outsider841@simplelogin.fr>
2025-11-06 18:56:19 +0200

    feat(audio): add option for media controls in panel

--- modules/audio/icedos.nix
+++ modules/audio/icedos.nix
@@ -14,11 +14,13 @@
         )
         outputAmplification
         inputAmplification
+        showMediaControlsInPanel
         ;
     in
     {
       outputAmplification = mkBoolOption { default = outputAmplification; };
       inputAmplification = mkBoolOption { default = inputAmplification; };
+      showMediaControlsInPanel = mkBoolOption { default = showMediaControlsInPanel; };
     };
 
   outputs.nixosModules =
@@ -37,6 +39,7 @@
           inherit (cosmic.audio)
             outputAmplification
             inputAmplification
+            showMediaControlsInPanel
             ;
 
           inherit (lib) mapAttrs;
@@ -45,6 +48,11 @@
         {
           home-manager.users = mapAttrs (user: _: {
             home.file = {
+              ".config/cosmic/com.system76.CosmicAppletAudio/v1/show_media_controls_in_top_panel" = {
+                inherit force;
+                text = if showMediaControlsInPanel then "true" else "false";
+              };
+
               ".config/cosmic/com.system76.CosmicAudio/v1/amplification_source" = {
                 inherit force;
                 text = if outputAmplification then "true" else "false";
