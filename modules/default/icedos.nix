d541a57b9fc69d57266bdaf436d51bc7f1c1ca68
IceDBorn <git.outsider841@simplelogin.fr>
2025-11-07 13:59:29 +0200

    feat(packages): exclude some defaults and add some extensions

--- modules/default/icedos.nix
+++ modules/default/icedos.nix
@@ -4,9 +4,25 @@
   outputs.nixosModules =
     { ... }:
     [
-      {
-        services.desktopManager.cosmic.enable = true;
-      }
+      (
+        { pkgs, ... }:
+
+        {
+          environment.systemPackages = with pkgs; [
+            cosmic-ext-applet-external-monitor-brightness
+            cosmic-ext-tweaks
+          ];
+
+          environment.cosmic.excludePackages = with pkgs; [
+            cosmic-edit
+            cosmic-player
+            cosmic-reader
+            cosmic-term
+          ];
+
+          services.desktopManager.cosmic.enable = true;
+        }
+      )
     ];
 
   meta.name = "default";
