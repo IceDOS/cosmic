5bd86ee4ef5a3bcec64e9797629c655fe803b05b
IceDBorn <git.outsider841@simplelogin.fr>
2025-11-16 02:55:22 +0200

    rework(time): use cosmic manager

--- modules/time/icedos.nix
+++ modules/time/icedos.nix
@@ -6,13 +6,20 @@
       inherit (icedosLib) mkBoolOption mkStrOption;
       inherit (lib) readFile;
 
-      inherit ((fromTOML (readFile ./config.toml)).icedos.desktop.cosmic.time) date firstDayOfTheWeek hourFormat24 seconds;
+      inherit ((fromTOML (readFile ./config.toml)).icedos.desktop.cosmic.time)
+        date
+        firstDayOfTheWeek
+        hourFormat24
+        seconds
+        weekday
+        ;
     in
     {
       date = mkBoolOption { default = date; };
       firstDayOfTheWeek = mkStrOption { default = firstDayOfTheWeek; };
       hourFormat24 = mkBoolOption { default = hourFormat24; };
       seconds = mkBoolOption { default = seconds; };
+      weekday = mkBoolOption { default = weekday; };
     };
 
   outputs.nixosModules =
@@ -34,39 +41,26 @@
                 firstDayOfTheWeek
                 hourFormat24
                 seconds
+                weekday
                 ;
+
               inherit (lib) mapAttrs;
-              force = true;
             in
             mapAttrs (user: _: {
-              home.file = {
-                ".config/cosmic/com.system76.CosmicAppletTime/v1/first_day_of_week" = {
-                  inherit force;
+              wayland.desktopManager.cosmic.applets.time.settings = {
+                first_day_of_week =
+                  {
+                    monday = 0;
+                    friday = 4;
+                    saturday = 5;
+                    sunday = 6;
+                  }
+                  .${firstDayOfTheWeek};
 
-                  text =
-                    {
-                      monday = "0";
-                      friday = "4";
-                      saturday = "5";
-                      sunday = "6";
-                    }
-                    .${firstDayOfTheWeek};
-                };
-
-                ".config/cosmic/com.system76.CosmicAppletTime/v1/military_time" = {
-                  inherit force;
-                  text = if hourFormat24 then "true" else "false";
-                };
-
-                ".config/cosmic/com.system76.CosmicAppletTime/v1/show_date_in_top_panel" = {
-                  inherit force;
-                  text = if date then "true" else "false";
-                };
-
-                ".config/cosmic/com.system76.CosmicAppletTime/v1/show_seconds" = {
-                  inherit force;
-                  text = if seconds then "true" else "false";
-                };
+                military_time = hourFormat24;
+                show_date_in_top_panel = date;
+                show_seconds = seconds;
+                show_weekday = weekday;
               };
             }) users;
         }
