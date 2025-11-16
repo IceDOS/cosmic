a717f143ad2a89433a6e47f510f3953ae20920d5
IceDBorn <git.outsider841@simplelogin.fr>
2025-11-16 22:15:28 +0200

    rework(power): use cosmic manager

--- modules/power/icedos.nix
+++ modules/power/icedos.nix
@@ -16,25 +16,27 @@
             let
               inherit (config.icedos.desktop) users;
               inherit (lib) mapAttrs;
-              force = true;
             in
-            mapAttrs (user: _: {
-              home.file =
-                let
-                  inherit (users.${user}.idle) disableMonitors suspend;
-                in
-                {
-                  ".config/cosmic/com.system76.CosmicIdle/v1/screen_off_time" = {
-                    inherit force;
-                    text = if (disableMonitors.enable) then "Some(${toString (disableMonitors.seconds * 1000)})" else "None";
-                  };
+            mapAttrs (
+              user: _:
+              let
+                inherit (config.home-manager.users.${user}.lib.cosmic) mkRON;
+                inherit (users.${user}.idle) disableMonitors suspend;
+                suspendSeconds = mkRON "raw" (
+                  if (suspend.enable) then "Some(${toString (suspend.seconds * 1000)})" else "None"
+                );
+              in
+              {
+                wayland.desktopManager.cosmic.idle = {
+                  screen_off_time = mkRON "raw" (
+                    if (disableMonitors.enable) then "Some(${toString (disableMonitors.seconds * 1000)})" else "None"
+                  );
 
-                  ".config/cosmic/com.system76.CosmicIdle/v1/suspend_on_ac_time" = {
-                    inherit force;
-                    text = if (suspend.enable) then "Some(${toString (suspend.seconds * 1000)})" else "None";
-                  };
+                  suspend_on_ac_time = suspendSeconds;
+                  suspend_on_battery_time = suspendSeconds;
                 };
-            }) users;
+              }
+            ) users;
         }
       )
     ];
